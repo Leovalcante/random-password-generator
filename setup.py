510a7d0cb981c8fe4c0ce71400e6fce5393b3a79
Valerio Preti <valerio.preti@gmail.com>
2019-06-05 01:03:28 +0200

    Update python version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "click>=7.0",
         "requests>=2.0"
     ],
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
+    python_requires=">=3.7",
     entry_points={
         "console_scripts": [
             "rpg = random_password_generator.rpg:rpg",
